7d2117a168a781ae335f7369b904dd51f170cd1b
hgbluemoon215-bit <hgbluemoon215@gmail.com>
2026-05-02 14:27:06 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// ============================================================
-// FIREBASE IMPORTS
-// ============================================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
   getAuth,
