b5c0cf818fa1b7792df07c24983307e4639d77f0
hgbluemoon215-bit <hgbluemoon215@gmail.com>
2026-07-14 14:35:22 +0530

    Create nash.php

---
+++ nash.php
@@ -0,0 +1,19 @@
+<?php
+$a=12;
+if($a>20)
+{
+  echo"a is greater than 20";
+}
+  elseif ($a>15)
+{
+  echo"a is greater than 15 but not greater than 20";
+}
+  elseif ($a>10)
+{
+  echo"a is greater than 10 but not greater than 15";
+}
+  else
+{
+  echo"a is not greater than 10";
+}
+?>
